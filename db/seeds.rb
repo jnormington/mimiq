903a8134930b0a82e5770c66965edfa4952295e0
Jon Normington <jnormington@users.noreply.github.com>
2015-06-13 23:39:27 +0100

    Update seeds and add example responses  - Update home page content  - Add home link on responses index page.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,30 +8,42 @@
 
 Card.find_or_create_by(title: 'Timeout') do |card|
   card.caption = 'Tests server response after preset time'
-  card.description = "The url allows you to do a GET request. It will respond after 5 seconds by default or specified time, try below"
+  card.description = "The url allows you to do a GET request.
+    It will respond after 5 seconds by default or specified time, try below"
   card.link = "/scenario/timeout?time=10"
 end
 
 Card.find_or_create_by(title: 'Not Found (404)') do |card|
   card.caption = 'Tests server responding with 404'
-  card.description = "The url allows you to do a GET request. It always responds with a 404 http status with the public/404.html page"
+  card.description = "The url allows you to do a GET request.
+    It always responds with a 404 http status with the public/404.html page"
   card.link = "/scenario/not_found"
 end
 
 Card.find_or_create_by(title: 'Server Error (500)') do |card|
   card.caption = 'Tests server responding with 500'
-  card.description = "The url allows you to do a GET request. It always responds with a 500 http status with the public/500.html page"
+  card.description = "The url allows you to do a GET request.
+    It always responds with a 500 http status with the public/500.html page"
   card.link = "/scenario/server_error"
 end
 
-Card.find_or_create_by(title: 'GET Request') do |card|
-  card.caption = 'Tests server responding with certain content'
-  card.description = "The url allows you to do a GET request. Its responds with your content you have configured. It can be JSON/XML or just plain old html"
-  card.link = "/scenario/get"
+Card.find_or_create_by(title: 'Manage custom responses') do |card|
+  card.caption = 'Create custom responses for get and post'
+  card.description = "The url allows you to do a POST request and it respond with your
+    content you have configured. It can be JSON/XML or just plain old html"
+  card.link = "/responses"
 end
 
-Card.find_or_create_by(title: 'POST Request') do |card|
-  card.caption = 'Tests server responding to your POST request'
-  card.description = "The url allows you to do a POST request and it respond with your content you have configured. It can be JSON/XML or just plain old html"
-  card.link = "/scenario/post"
-end
+Response.create!(
+  response_type: 'JSON',
+  content: "{blank: 'yes'}",
+  request_type: 'GET',
+  request_by: 'test'
+)
+
+Response.create!(
+  response_type: '500',
+  content: 'never used but something required as /public/500.html used',
+  request_type: 'POST',
+  request_by: 'test'
+)
